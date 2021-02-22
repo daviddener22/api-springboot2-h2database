48a10d2caf533bbf5143c7011e4436eccdc76b4e
daviddener22 <david.dener@ebix.com>
2021-02-22 16:47:37 -0300

    Added the method findById

--- src/main/java/com/david/myapi/resources/UserResource.java
+++ src/main/java/com/david/myapi/resources/UserResource.java
@@ -19,9 +19,7 @@ public class UserResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-
 		User user = userService.findById(id);
-
 		return ResponseEntity.ok().body(user);
 
 	}
